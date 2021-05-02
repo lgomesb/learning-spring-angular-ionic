9d2760d8b3d7db35506a4b4e904843d854f9e77d
leandro <lgomesb@gmail.com>
2021-05-02 17:59:57 -0300

    Produto and Categoria associated Many-to-Many

--- src/main/java/com/learning/cursomc/domain/Produto.java
+++ src/main/java/com/learning/cursomc/domain/Produto.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import lombok.EqualsAndHashCode;
@@ -20,22 +22,28 @@ import lombok.Setter;
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue( strategy = GenerationType.IDENTITY)
-	private Integer id;	
-	private String name;
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name; 
+	private Double preco;
+	
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA", 
+		joinColumns = @JoinColumn(name = "produto_id"),
+		inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
 
-	public Categoria(Integer id, String name) {		
+	public Produto(Integer id, String name, Double preco) {		
 		this.id = id;
 		this.name = name;
+		this.preco = preco;
 	}
 	
 	
+
 }
