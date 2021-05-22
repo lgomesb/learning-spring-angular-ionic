1f600461c174a290a64de531f09656c47cb3ebee
leandro <lgomesb@gmail.com>
2021-05-22 18:53:51 -0300

    Remove JsonManagedReference and JsonBackReference

--- src/main/java/com/learning/cursomc/domain/Cliente.java
+++ src/main/java/com/learning/cursomc/domain/Cliente.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.learning.cursomc.domain.enums.TipoCliente;
 
 import lombok.EqualsAndHashCode;
@@ -40,7 +39,6 @@ public class Cliente implements Serializable {
 	private String cprOuCnpf;
 	private Integer tipo;
 	
-	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
@@ -48,7 +46,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos =  new ArrayList<>();
 
--- src/main/java/com/learning/cursomc/domain/Pagamento.java
+++ src/main/java/com/learning/cursomc/domain/Pagamento.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.learning.cursomc.domain.enums.EstadoPagamento;
 
 import lombok.AllArgsConstructor;
@@ -34,7 +34,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
