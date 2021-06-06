1c9b146c1c05c754cb0b61bbd22adb2bd2b10a59
leandro <lgomesb@gmail.com>
2021-06-06 17:17:21 -0300

    Implements Bucket AWS

--- src/main/java/com/learning/cursomc/CursomcApplication.java
+++ src/main/java/com/learning/cursomc/CursomcApplication.java
@@ -1,11 +1,17 @@
 package com.learning.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.learning.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -14,7 +20,7 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		
+		s3Service.uploadFile("/home/leandro/Projetos/aws/curso-spring-ionic/images/jose.png");
 	}
 
 }
